9856f0772438a6639ec4398295e58758ed04662e
1_1_LJS <jeongseob.korea@gmail.com>
2022-07-29 17:19:41 +0900

    3rd project finished 1qn

--- 1회차/이정섭/0_최빈수구하기.py
+++ 1회차/이정섭/0_최빈수구하기.py
@@ -1,3 +1,11 @@
-import sys
 
-sys.stdin = open("_최빈수구하기.txt")
+t = int(input()) # input # of total cases
+n = int(input()) # Input the # of test case
+score_list = [] # make list for upcoming score input
+for scores in range (t): 
+    score = map(int, input().split()) # acknowlege inputs as integer
+    score_list.append(score)
+    score_list.count(score) # use count method to figure out mos frequent score
+                            # try to use length method on count.
+
+print(t,f'#{n} {score}')    # Use f'string to print out both number of test case and the frequent score.
--- 1회차/이정섭/3_직사각형길이찾기.py
+++ 1회차/이정섭/3_직사각형길이찾기.py
@@ -1,3 +1,19 @@
-import sys
+t = int(input())
+a,b,c = input().split()
 
-sys.stdin = open("_직사각형길이찾기.txt")
+# Undefined side 'd' is either a,b or c
+# if a = b != c then d is c
+# if a = b = c then d is a
+# if a != b and a = c then d is b
+
+def d(): 
+    if a == b and a != c:
+        print(c)
+    elif a == c and a != b:
+        print(b)
+    elif a == b and a == c:
+        print(a)
+    return
+
+for r in range (1, t+1): # range should be 1 to 't' and because python count from 0, it should be 't+1'
+    print(f'#{r} {d()}')
